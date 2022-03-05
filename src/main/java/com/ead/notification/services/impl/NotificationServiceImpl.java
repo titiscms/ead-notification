1ecfdc1a0e6276ce88c788bb2bba01d1f150f409
titiscms <titisbackup@gmail.com>
2022-03-04 22:29:40 -0300

    Consumer creation and exchange, queue and routing key configuration

--- src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,8 +1,11 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -11,5 +14,11 @@ public class NotificationServiceImpl implements NotificationService {
 
     public NotificationServiceImpl(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
+    }
+
+    @Transactional
+    @Override
+    public NotificationModel saveNotification(NotificationModel notificationModel) {
+        return notificationRepository.save(notificationModel);
     }
 }
